dd487cc3efbecdbb31dad6ce423adc3581d67df8
andres0818 <c.andres.perez.h@gmail.com>
2023-02-07 17:40:57 -0500

    Add icons required for app

---
+++ src/img/index.js
@@ -0,0 +1,21 @@
+import Background from './Background.svg'
+import Correo from './Correo.svg'
+import Edit from './Edit.svg'
+import Key from './Key.svg'
+import Logo from './Logo.svg'
+import Printer from './Printer.svg'
+import User from './User.svg'
+import UserGray from './UserGray.svg'
+import ImgIcon from './ImgIcon.svg'
+
+export {
+    Background
+    , Correo
+    , Edit
+    , Key
+    , Logo
+    , Printer
+    , User
+    , UserGray
+    , ImgIcon
+}
