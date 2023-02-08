724e1ac0fabddbcef057192f2b53f4d7cf6b0074
andres0818 <c.andres.perez.h@gmail.com>
2023-02-08 00:12:42 -0500

    feat: add useContex

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import UserProvider from './components/context/UserContext';
+import UserProvider from './context/UserContext';
 
 
 const root = ReactDOM.createRoot(
