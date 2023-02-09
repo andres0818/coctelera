de649579c1236bc6247a68046a2ce7a6b8482374
andres0818 <c.andres.perez.h@gmail.com>
2023-02-09 17:45:23 -0500

    feat: navbar sticky

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { NavigateFunction, useNavigate } from 'react-router-dom'
 import './Navbar.scss'
 
