3c32b35d5d282de3c56950f1f9c085f0c80f9143
andres0818 <c.andres.perez.h@gmail.com>
2023-02-11 13:43:18 -0500

    feat: layout and create structue

--- src/components/ReporteVentas/ReporteVentas.tsx
+++ src/components/ReporteVentas/ReporteVentas.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import './ReporteVentas.scss'
 const ReporteVentas = () => {
+
   return (
-    <div>ReporteVentas</div>
+    <div className='reporteVentas'>
+      <p className='reporteVentas__title'>Reporte Ventas Diarias</p>
+      <h1 className='reporteVentas__subTitle'>Ventas por mesas</h1>
+      <p className='reporteVentas__fecha'>fecha</p>
+
+      <div className='reporteVentas__containerCards'>
+        <div className='reporteVentas__cards'>
+          <h3>mesa</h3>
+          <h3>$800.000</h3>
+        </div>
+      </div>
+        <div className='reporteVentas__cardsTotal'>
+          <h3>Total</h3>
+          <h3>800.000</h3>
+        </div>
+      <button className='reporteVentas__btnTotal'>
+        <span>Total Ventas</span>
+        <span>$800.000</span>
+      </button>
+    </div>
   )
 }
 
